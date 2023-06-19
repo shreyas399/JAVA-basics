7bed28f9aa1f877cc32943c12575551009002799
shreyas399 <129728792+shreyas399@users.noreply.github.com>
2023-06-19 19:20:04 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,12 @@
+class array
+{
+  
+    public static void main(String a[])
+    {
+        int nums[]={4,5,6,8,9,10,20,50};
+        for(int i=0;i<nums.length;i++)
+        {
+        System.out.println(nums[i]);
+        }
+    }
+}
