e64e26f03eed39119a8c84bc6dea13ab7e8e277d
shreyas399 <129728792+shreyas399@users.noreply.github.com>
2023-08-03 16:07:25 +0530

    Create Hollow patterns.java

---
+++ Hollow patterns.java
@@ -0,0 +1,29 @@
+
+class demo
+{
+    public static void main(String []args)
+    {
+    
+    int a=5;
+    int b=5;
+
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=b;j++)
+        {
+            if(i==1 || j==1|| i==a || j==b)
+            {
+            System.out.print("*"+" ");
+            }
+            else
+            {
+              System.out.print(" "+" ");
+            }
+        }
+        System.out.println();
+    }
+       
+
+       
+    }
+}
